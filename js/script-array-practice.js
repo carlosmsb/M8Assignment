f434458926a47b92dd732f625c65db12b9fd43b9
Admin <>
2023-11-19 15:21:51 -0800

    7-11Commit

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -32,21 +32,38 @@ for (let index in movies) {
 let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope', 'Alien', 'Shrek'];
 for (let movie of movies) {
     console.log(movie);
-} */
+} 
 //STEP 7
 let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope', 'Alien', 'Shrek'];
 movies.sort();
 for (let movie of movies) {
     console.log(movie);
-}
+}  */
 //STEP 8
+let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope', 'Alien', 'Shrek'];
+let leastFavMovies = ['Annabelle 2', 'The Pyramid', 'Zootopia'];
+console.log('Movies I like:');
+for (let movie of movies) {
+  console.log(movie);
+}
 
+console.log('\nMovies I regret watching:');
+for (let regretMovie of leastFavMovies) {
+  console.log(regretMovie);
+}
 //STEP 9
-
+let allMovies = movies.concat(leastFavMovies);
+allMovies.sort().reverse();
+console.log('\nCombined Movies (Reverse Sorted):');
+for (let movie of allMovies) {
+    console.log(movie);
+}
 //STEP 10
-
+//let lastMovie = allMovies[allMovies.length - 1];
+//console.log('\nLast Movie in the Combined Array:', lastMovie);
 //STEP 11
-
+let firstMovie = allMovies.shift();
+console.log('\nFirst Movie in the Combined Array:', firstMovie);
 //STEP 12
 
 //STEP 13
