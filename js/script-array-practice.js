d217a50b847c7ebb1a52910a04539ad957e3ed33
Admin <>
2023-11-19 13:04:38 -0800

    initCommit

---
+++ js/script-array-practice.js
@@ -0,0 +1,34 @@
+//STEP 1
+const favoriteMovies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope'];
+console.log(favoriteMovies[1]);
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
