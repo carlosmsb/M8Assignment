1592c674ac1b7f8452f70ef47ffcf893d62b4240
Admin <>
2023-11-19 14:11:23 -0800

    1-4Complete

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -1,12 +1,28 @@
+/*
 //STEP 1
 const favoriteMovies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope'];
 console.log(favoriteMovies[1]);
 //STEP 2
-
+const movies = new Array(5);
+movies[0] = 'Alien';
+movies[1] = 'Frankenweenie';
+movies[2] = 'Akira';
+movies[3] = 'Funny Games';
+movies[4] = 'Rosemarys Baby';
+console.log(movies[0]);
 //STEP 3
-
+//const movies = new Array();
+movies[0] = 'Alien';
+movies[1] = 'Frankenweenie';
+movies[2] = 'Akira';
+movies[3] = 'Funny Games';
+movies[4] = 'Rosemarys Baby';
+movies[2] = 'Shrek'
+console.log(movies.length); 
 //STEP 4
-
+let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope'];
+delete movies[0];
+console.log(movies); */
 //STEP 5
 
 //STEP 6
