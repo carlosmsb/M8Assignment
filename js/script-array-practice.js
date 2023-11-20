8deabdf8f4f1aa6db0dc26b8f0238a97c0aec0f0
Admin <>
2023-11-19 16:21:31 -0800

    part1Complete

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -38,7 +38,7 @@ let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope', 
 movies.sort();
 for (let movie of movies) {
     console.log(movie);
-}  */
+}  
 //STEP 8
 let movies = ['Evil Dead', 'Hereditary', 'The Witch', 'La Belle Verte', 'Nope', 'Alien', 'Shrek'];
 let leastFavMovies = ['Annabelle 2', 'The Pyramid', 'Zootopia'];
@@ -62,16 +62,70 @@ for (let movie of allMovies) {
 //let lastMovie = allMovies[allMovies.length - 1];
 //console.log('\nLast Movie in the Combined Array:', lastMovie);
 //STEP 11
-let firstMovie = allMovies.shift();
-console.log('\nFirst Movie in the Combined Array:', firstMovie);
+//let firstMovie = allMovies.shift();
+//console.log('\nFirst Movie in the Combined Array:', firstMovie);
 //STEP 12
+let indicesOfMoviesToRemove = ['Annabelle 2', 'The Pyramid', 'Zootopia'];
+leastFavMovies.forEach(movie => {
+    let index = movies.indexOf(movie);
+    if (index !== -1) {
+        indicesOfMoviesToRemove.push(index);
+    }
+});
+console.log('\nIndices of Movies I Do Not Like:', indicesOfMoviesToRemove);
 
-//STEP 13
+let moviesToAdd = ['Alien', 'Frankenweenie', 'Akira'];
+indicesOfMoviesToRemove.forEach(index => {
+    movies[index] = moviesToAdd.shift();
+});
+console.log('\nUpdated Movies Array:', movies);
+//STEP 13 
+const movies = [
+    ["the warriors", 1],
+    ["funny games", 2],
+    ["deathproof", 3],
+    ["donnie darko", 4],
+    ["mad max", 5]
+];
+const movieNames = movies
+  .filter(([name]) => typeof name === 'string')
+  .map(([name]) => name);
+  
+  console.log(movieNames);
 
 //STEP 14
-
+const employees = ["ZAK", "JESSICA", "MARK", "FRED", "SALLY"];
+const showEmployee = function (employeeArray) {
+    console.log("Employees:");
+  
+    for (let i = 0; i < employeeArray.length; i++) {
+      console.log(employeeArray[i]);
+    }
+  };
+  
+  showEmployee(employees);
 //STEP 15
-
+function filterValues(array) {
+  return array.filter(value => {
+    return value !== false && value !== null && value !== 0 && value !== '';
+  });
+}
+console.log(filterValues([58, '', 'abcd', true, null, false, 0]));
 //STEP 16
-
+function getRandomNumber(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+  const numericArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const randomItem = getRandomNumber(numericArray);
+  
+  console.log(randomItem);
+  */
 //STEP 17
+function getLargestNumber(array) {
+    return Math.max(...array);
+  }
+  const numericArray = [3, 16, 9, 33, 1];
+  const largestNumber = getLargestNumber(numericArray);
+  
+  console.log(largestNumber);
